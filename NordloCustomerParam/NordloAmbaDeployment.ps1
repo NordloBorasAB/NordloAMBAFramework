b9ee1f6d661f4f137b62cb53bca276c5af7ef5a9
andreashodnebrug <andreas.hodnebrug@nordlo.com>
2025-04-02 11:08:52 +0200

    Fixed definitions

--- NordloCustomerParam/NordloAmbaDeployment.ps1
+++ NordloCustomerParam/NordloAmbaDeployment.ps1
@@ -8,7 +8,7 @@ $pseudoRootManagementGroup = "MuovitechAB"
 
 az deployment mg create `
     --name "amba-Deployment-01" `
-    --template-uri https://raw.githubusercontent.com/azure/azure-monitor-baseline-alerts/refs/heads/main/patterns/alz/alzArm.param.json `
+    --template-uri https://raw.githubusercontent.com/azure/azure-monitor-baseline-alerts/refs/heads/main/patterns/alz/alzArm.json `
     --location $location `
     --management-group-id $pseudoRootManagementGroup `
     --parameters "NordloAMBAFramework\NordloCustomerParam\alzArm.param_NordloCustomMuovitech_renew.json"
